5aafb74507016338cfb172d1b3b3f90252418da0
DParadoxus <and.31@mail.ru>
2017-03-07 14:55:58 +0300

    Initial Commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/user/djangoenv/bin/python3.5
+# EASY-INSTALL-SCRIPT: 'Django==1.10.6','django-admin.py'
+__requires__ = 'Django==1.10.6'
+__import__('pkg_resources').run_script('Django==1.10.6', 'django-admin.py')
